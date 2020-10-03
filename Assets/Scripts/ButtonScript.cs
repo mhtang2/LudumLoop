d2cbf0fde83d99e7f6c0fe0fd725e565aa36a912
Sparsh <ss85@illinois.edu>
2020-10-03 17:53:13 -0500

    endgame panel

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonScript : MonoBehaviour
+{
+    // Buttons poggers
+    public void startButton()
+    {
+        SceneManager.LoadScene("Matty");
+        
+    }
+
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene("Options");
+
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene("Credits");
+
+    }
+
+    public void RestartButton()
+    {
+        int y = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(y);
+
+    }
+
+    public void LevelSelectButton()
+    {
+        SceneManager.LoadScene("LevelSelect");
+
+    }
+
+    public void NextLevelButton()
+    {
+        int y = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(y + 1);
+
+    }
+}
