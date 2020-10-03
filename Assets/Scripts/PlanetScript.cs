c8c45fb828ecdc1103150225f9d3441cf165b191
gemeln <mtang317@gmail.com>
2020-10-02 20:46:52 -0500

    player

--- Assets/Scripts/PlanetScript.cs
+++ Assets/Scripts/PlanetScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlanetScript : MonoBehaviour
 {
+    public int mass;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,17 +8,30 @@ public class PlayerScript : MonoBehaviour
     //Member variables
     private bool IsOrbited;
     private Rigidbody2D PlayerRigidBody;
-
+    public PlanetScript planet;
+    public int mass;
+    public float vi;
+    public float G;
     // Start is called before the first frame update
+    float GM;
+    private Vector3 v;
     void Start()
     {
         PlayerRigidBody = gameObject.GetComponent<Rigidbody2D>();
+        PlayerRigidBody.velocity = new Vector3(0,vi,0);
+        v = new Vector3(0, vi,0);
+        GM =    G * planet.mass;
+        Debug.Log(GM);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 r = planet.transform.position - transform.position;
+        Vector3 a = GM * r / (r.magnitude * r.magnitude * r.magnitude);
+        transform.position = transform.position + v * Time.deltaTime;
+        v += Time.deltaTime * a;
+
     }
 
     public void FlipIsOrbited()
