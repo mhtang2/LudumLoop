775786b99fa831df83173ce839527c8b77a201be
firstdreamding <huaxu3721@yahoo.com>
2020-10-03 15:23:01 -0500

    Added basic time control

---
+++ Assets/GameSpeed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSpeed : MonoBehaviour
+{
+    int gameSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSpeed = 1;
+        Time.timeScale = gameSpeed;
+    }
+
+    public void Increase()
+    {
+        gameSpeed *= 2;
+        Time.timeScale = gameSpeed;
+    }
+
+    public void Decrease()
+    {
+        gameSpeed /= 2;
+        Time.timeScale = gameSpeed;
+    }
+}
