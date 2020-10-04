0f723bf1f58551788aa89f5a04aa099a24a8f3b2
firstdreamding <huaxu3721@yahoo.com>
2020-10-04 18:13:19 -0500

    Added Speed indicator

--- Assets/GameSpeed.cs
+++ Assets/GameSpeed.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSpeed : MonoBehaviour
 {
     float gameSpeed;
+    Text displayText;
 
     // Start is called before the first frame update
     void Start()
     {
         gameSpeed = 1;
         Time.timeScale = gameSpeed;
+        displayText = GetComponent<Text>();
+        displayText.text = gameSpeed + "x";
     }
 
     public void Increase()
@@ -27,8 +31,9 @@ public class GameSpeed : MonoBehaviour
     }
 
     private void SetGameSpeed()
-    {
+    { 
         gameSpeed = Mathf.Clamp(gameSpeed, 0.5f, 8f);
+        displayText.text = gameSpeed + "x";
         Time.timeScale = gameSpeed;
     }
 }
